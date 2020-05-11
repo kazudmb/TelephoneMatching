b91ab69fbbb09b1dd3302f51965bb0cab439dd23
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-05-11 09:06:30 +0900

    add AdMob

--- app/src/main/java/com/nakano/telephonematching/MainActivity.kt
+++ app/src/main/java/com/nakano/telephonematching/MainActivity.kt
@@ -2,11 +2,21 @@ package com.nakano.telephonematching
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var mAdView : AdView
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111")
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
